c544810d1ead3a7325c4f3011a5a41b251ee46dc
Hristo <hd0891@my.bristol.ac.uk>
2018-03-15 16:51:25 +0000

    Added new split page

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+// Left Panel
+left.addEventListener('mouseenter', () => {
+container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () => {
+container.classList.remove('hover-left');
+});
+
+// Right Panel
+right.addEventListener('mouseenter', () => {
+container.classList.add('hover-right');
+});
+
+right.addEventListener('mouseleave', () => {
+container.classList.remove('hover-right');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
